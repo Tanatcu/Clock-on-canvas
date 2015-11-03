15c23b3746c4b27d4ba1fcc9a1398c4364a72f32
Ruslan Loboda <ruslan.iuko777@mail.ru>
2015-11-03 15:05:19 +0200

    полупроцесс

--- js/index.js
+++ js/index.js
@@ -1,37 +1,22 @@
-// Внутренние переменные
 var canvas, ctx;
 var clockRadius;
 var hmc,sc,fc,cfc;
-function use() {
+
+function getcolor() {
     sc = document.getElementById('scolor').value;
     hmc = document.getElementById('hmcolor').value;
     fc = document.getElementById('fcolor').value;
     cfc = document.getElementById('cfcolor').value;
 }
+
 function clear() {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
+        
 function drawScene() {
     clear();
-    use();
+    getcolor();
 
-    //Внешний круг
-    ctx.beginPath();
-    ctx.strokeStyle = fc;
-    ctx.lineWidth = '60';
-    ctx.arc(clockRadius,clockRadius,clockRadius*0.8,0,2*Math.PI);
-    ctx.stroke();
-    ctx.closePath();
-
-    // Внутрений круг
-    ctx.beginPath();
-    ctx.fillStyle = fc;
-    ctx.lineWidth = '30';
-    ctx.arc(clockRadius,clockRadius,clockRadius*0.3,0,2*Math.PI);
-    ctx.stroke();
-    ctx.closePath();
-
-    // Вытаскивает текущее время
     var date = new Date();
     var hours = date.getHours();
     var minutes = date.getMinutes();
@@ -41,10 +26,21 @@ function drawScene() {
     var minute = minutes + seconds / 60;
 
     ctx.save();
-    ctx.translate(canvas.width/2, canvas.height/2);
+    ctx.translate(clockRadius, clockRadius);
+    
     ctx.beginPath();
-
-    // Циферблат
+    ctx.strokeStyle = fc;
+    ctx.lineWidth = '30';
+    ctx.arc(0,0,clockRadius*0.3,0,2*Math.PI);
+    ctx.stroke();
+    
+    ctx.beginPath();
+    ctx.strokeStyle = fc;
+    ctx.lineWidth = '60';
+    ctx.arc(0,0,clockRadius*0.8,0,2*Math.PI);
+    ctx.stroke();
+    
+    ctx.beginPath();
     ctx.font = '38px Arial';
     ctx.textAlign = 'center';
     ctx.fillStyle = cfc;
@@ -55,22 +51,25 @@ function drawScene() {
         var y = clockRadius * 0.8 * Math.sin(theta);
         ctx.fillText(n, x, y);
     }
+    ctx.closePath();
 
-    // Часовая стрелка
-    ctx.save();
+    //ctx.save();
     var thetato = (hour - 3) * 2 / 12 * Math.PI ;
     ctx.rotate(thetato);
     ctx.beginPath();
     ctx.moveTo(-15, -5);
     ctx.lineTo(-15, 5);
-    ctx.lineTo(clockRadius * 0.5, 1);
-    ctx.lineTo(clockRadius * 0.5, -1);
+    ctx.lineTo(clockRadius * 0.55, 1);
+    ctx.lineTo(clockRadius * 0.55, -1);
+    ctx.closePath();
     ctx.fillStyle = hmc;
     ctx.fill();
-    ctx.restore();
-
-    // Минутная стрелка
-    ctx.save();
+    //ctx.restore();
+    
+    
+    
+    
+    //ctx.save();
     var thet = (minute - 15) * 2 / 60 * Math.PI;
     ctx.rotate(thet);
     ctx.beginPath();
@@ -78,22 +77,23 @@ function drawScene() {
     ctx.lineTo(-15, 4);
     ctx.lineTo(clockRadius * 0.8, 1);
     ctx.lineTo(clockRadius * 0.8, -1);
+    ctx.closePath();
     ctx.fillStyle = hmc;
     ctx.fill();
-    ctx.restore();
+    //ctx.restore();
 
-    // Секундная стрелка
-    ctx.save();
+    //ctx.save();
     var the = (seconds - 15) * 2 / 60 * Math.PI;
     ctx.rotate(the);
     ctx.beginPath();
     ctx.moveTo(-15, -3);
     ctx.lineTo(-15, 3);
     ctx.lineTo(clockRadius * 0.9, 1);
-    ctx.lineTo(clockRadius * 0.9, -1);
+    ctx.lineTo(clockRadius * 0.9, 0);
+    ctx.closePath();
     ctx.fillStyle = sc;
     ctx.fill();
-    ctx.restore();
+    //ctx.restore();
     ctx.restore();
 }
 $(function() {
